207174fc836347b307a1a529429721580f21d337
caromain <romain.carrere@ynov.com>
2026-02-18 13:59:31 +0100

    feat(BankAccount): rework

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        BankAccount ZooBankAccount = new BankAccount();
+        ZooBankAccount.Sell(80000);
+        if (ZooBankAccount.hasError == true)
+        {
+            Console.WriteLine(ZooBankAccount.errorString);
+        }
+    }
+}
