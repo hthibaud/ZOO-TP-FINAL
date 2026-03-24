dc444589c89c83eecae3ed08a7da82d558b71b79
hthibaud <thibaud.herry@ynov.com>
2026-03-24 17:26:10 +0100

    refactor(feat/habitat): fixed some ancient comments here

--- eagle_habitat.cs
+++ eagle_habitat.cs
@@ -1,9 +1,5 @@
 public class Eagle_Habitat : Habitat
 {
-    // public int numberOfEagles {get; private set;}
-    // private int _maxEagles = 4;
-
-    // private int _price = 2000;
     public Eagle_Habitat() : base (0, 4, 2000)
     {
     }
--- tiger_habitat.cs
+++ tiger_habitat.cs
@@ -1,11 +1,5 @@
 public class Tiger_Habitat : Habitat
 {
-
-    // public int numberOfTigers {get; private set;}
-    // private int _maxTigers = 2;
-
-    // private int _price = 2000;
-
     public Tiger_Habitat() : base(0, 2, 2000)
     {
     }
